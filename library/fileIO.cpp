0da0042e0203fcac762998edd6254631c2a6d27a
Hope Webb <hope.webb.16@cnu.edu>
2019-02-15 15:38:51 -0500

    two functions

--- library/fileIO.cpp
+++ library/fileIO.cpp
@@ -1,4 +1,11 @@
 #include "../includes_usr/fileIO.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
 using namespace std;
 /* clears, then loads books from the file filename
  * returns  COULD_NOT_OPEN_FILE if cannot open filename
@@ -7,6 +14,39 @@ using namespace std;
  * */
 int loadBooks(std::vector<book> &books, const char* filename)
 {
+	int count = 0;
+	int count2 = 0;
+	ifstream myfile;
+	vector<string> tempVector;
+	book tempBook;
+
+	myfile.open(filename);
+	string line;
+	if(!myfile.is_open()){
+		return COULD_NOT_OPEN_FILE;
+	}
+
+	while(getline(myfile,line)){
+		istringstream lineStream(line);
+		string space;
+		while (getline(lineStream, space,',')){
+			tempVector.at(count)=line;
+			count++;
+		}
+		tempBook.book_id= stoi(tempVector[0]);
+		tempBook.author = tempVector[1];
+		tempBook.state = (book_checkout_state) stoi(tempVector[3]);
+		tempBook.loaned_to_patron_id = stoi(tempVector[4]);
+		books.at(count2)= tempBook;
+		count2++;
+	}
+
+	if (books.empty()){
+		myfile.close();
+		return NO_BOOKS_IN_LIBRARY;
+	}
+
+	myfile.close();
 	return SUCCESS;
 }
 
@@ -17,6 +57,21 @@ int loadBooks(std::vector<book> &books, const char* filename)
  * */
 int saveBooks(std::vector<book> &books, const char* filename)
 {
+	ofstream myfile;
+	myfile.open(filename);
+
+	if (!myfile.is_open()){
+		 return COULD_NOT_OPEN_FILE;
+	}
+	if(books.size() == 0){
+		myfile.close();
+		return NO_BOOKS_IN_LIBRARY;
+	}
+	std::vector<book>::iterator it;
+	for (it = books.begin(); it != books.end(); it++){
+		 myfile << (*it).book_id <<"," << (*it).title << "," << (*it).author << "," << (*it).state << "," << (*it).loaned_to_patron_id << endl;
+	}
+	myfile.close();
 	return SUCCESS;
 }
 
